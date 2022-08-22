efe58de5fb66dc354b36ed8f76084a0497766b2b
Ilias Karim <ilias.karim@gmail.com>
2022-08-22 15:07:07 -0400

    Add abbrev flag to ls-remote usage

--- builtin/ls-remote.c
+++ builtin/ls-remote.c
@@ -8,7 +8,7 @@
 static const char * const ls_remote_usage[] = {
 	N_("git ls-remote [--heads] [--tags] [--refs] [--upload-pack=<exec>]\n"
 	   "              [-q | --quiet] [--exit-code] [--get-url]\n"
-	   "              [--symref] [<repository> [<refs>...]]"),
+	   "              [--symref] [--abbrev] [<repository> [<refs>...]]"),
 	NULL
 };
 
